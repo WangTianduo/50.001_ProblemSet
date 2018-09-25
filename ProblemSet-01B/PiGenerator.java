a914d35db5e92dda49492f2a28107e82884f5768
Tianduo Wang <wtianduo@gmail.com>
2018-09-25 14:59:42 +0800

    small change

--- ProblemSet-01B/PiGenerator.java
+++ ProblemSet-01B/PiGenerator.java
@@ -38,7 +38,7 @@ public class PiGenerator {
      */
     public static int powerMod(int a, int b, int m) {
         // TODO: Implement (Problem a)
-        if((a < 0 && b < 0) || m <= 0) {
+        if((a < 0 || b < 0) || m <= 0) {
             return -1;
         }
         return ((int)Math.pow(a, b)) % m;
